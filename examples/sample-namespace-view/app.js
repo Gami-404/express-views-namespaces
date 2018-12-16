0f012394cccf3d103a3cf49c2a541acb12e582f3
Abdulrhman Gamal <abdo.gamy2010@gmail.com>
2018-12-16 18:23:56 +0200

    Add First exmaples

---
+++ examples/sample-namespace-view/app.js
@@ -0,0 +1,54 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+//1- namespaces packages creator
+var namespace=require('./express-views-namespaces');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 2- step one send express app as paramneter 
+namespace(app);
+
+
+// 3 - create new namespace for view 
+app.namespaceView('posts', path.join(__dirname, 'posts', 'view'));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+
+module.exports = app;
---
+++ examples/sample-namespace-view/posts/view/index.hbs
@@ -0,0 +1,1 @@
+<p>Customer</p>
